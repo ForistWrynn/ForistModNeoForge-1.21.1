d18be551f9b33ecdd41c9969773181e891889b84
ForistWrynn <jittiwatchada@gmail.com>
2026-03-17 11:19:05 +0000

    World Gen with Data Gen

---
+++ src/main/java/net/forist/forist_mod/worldgen/ModBiomeModifiers.java
@@ -0,0 +1,26 @@
+package net.forist.forist_mod.worldgen;
+
+import net.forist.forist_mod.ForistMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+public class ModBiomeModifiers {
+
+    public static void bootstrap(BootstrapContext<BiomeModifier> context) {
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+
+
+    }
+
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ResourceLocation
+                .fromNamespaceAndPath(ForistMod.MOD_ID, name));
+    }
+}
